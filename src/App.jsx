478a26ade87a9a05fa01d148c48bf312500b69b9
Darya Kolotilina <colotilina.d@yandex.ru>
2023-11-11 21:13:35 +0200

    "перед объединением"

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import EndOfGame from "./components/EndOfGame/EndOfGame";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
+import NavBar from "./components/NavBar/NavBar";
 import Rules from "./components/Rules/Rules";
 
 function App() {
   return (
     <>
       <Header />
+      <NavBar />
       <Main />
       <Rules />
       <EndOfGame />
