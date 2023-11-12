a748d5d2d327cd1f3d09d6554ccb23df3ef18859
Dima <45449261+kitdim@users.noreply.github.com>
2023-11-12 15:33:43 +0300

    Update DatabaseLoader.java

--- app/src/main/java/team44/com/app/DatabaseLoader.java
+++ app/src/main/java/team44/com/app/DatabaseLoader.java
@@ -4,10 +4,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import team44.com.app.domain.User;
 import team44.com.app.repository.UserRepository;
+import com.github.javafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
 
 @Component
 public class DatabaseLoader implements CommandLineRunner {
     private final UserRepository repository;
+    private final Faker faker = new Faker(Locale.getDefault());
 
     public DatabaseLoader(UserRepository repository) {
         this.repository = repository;
@@ -15,7 +21,10 @@ public class DatabaseLoader implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-        this.repository.save(new User("test1", "132164", "test@gmail.com", 0));
+        for (var i = 0; i < 100; i++) {
+            this.repository.save(new User(faker.name().username(), faker.internet().password(),
+                    faker.number().numberBetween(0, 100)));
+        }
     }
 
 }
